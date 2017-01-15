d8c05f342a8831166fefa63e0f3a47e82e5eaf86
Gwolf2u <gwolf2u.website@gmail.com>
2017-01-15 21:25:42 +0200

    h850: Update product name
    h850: update product name
    
    Change-Id: I2fe1c76453f27dce78d402413f24d0188e1b7472
    h850: update product name

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/lge/h850/full_h850.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
@@ -10,6 +10,6 @@ PRODUCT_NAME := lineage_h850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g5" \
-    PRODUCT_NAME="h1_tmo_us" \
+    PRODUCT_NAME="h1_global_com" \
     BUILD_FINGERPRINT="lge/h1_global_com/h1:7.0/NRD90U/163041049db12:user/release-keys" \
     PRIVATE_BUILD_DESC="h1_global_com-user 7.0 NRD90U 163041049db12 release-keys"
