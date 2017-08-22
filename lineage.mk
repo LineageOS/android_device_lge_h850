fc2e324c2d4650f5c1b8cc56fd709ab2a2620caf
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:31:55 -0700

    h850: vendor/cm -> vendor/lineage
    Change-Id: I0c4dfe17c28973798b0e470ed560db22f0352598

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/lge/h850/full_h850.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
