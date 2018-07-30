b6334d594d41fa8a89509b408f1a22ca77222c99
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-30 10:38:01 -0700

    h850: Move sensor config to vendor
    Change-Id: I48cbc289f9daad1d1cdf53e8ea62eec4b71ce9ed

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/lge/g5-common/g5.mk)
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_h1_global_com.conf:system/etc/sensors/sensor_def_variable.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_h1_global_com.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_variable.conf
 
 # WiFi Calibration
 PRODUCT_COPY_FILES += \
