c740871c41f8b8b332ce12a4a1d0a61024fb269c
Eric Meddaugh <eric@meddaughs.com>
2018-08-08 07:24:34 -0400

    h850: Add TARGET_SYSTEM_PARTITION for system-as-root layout
    Change-Id: Ie9ee55ac0677623c194f6e2ce7547e1aee2e5488

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ DEVICE_PATH := device/lge/h850
 -include device/lge/g5-common/BoardConfigCommon.mk
 
 TARGET_OTA_ASSERT_DEVICE := g5,h1,h850
+TARGET_SYSTEM_PARTITION := /dev/sda13
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_h850_defconfig
