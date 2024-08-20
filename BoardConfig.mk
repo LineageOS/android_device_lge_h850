3f66c21624737dff971be9bba56807911b0c2935
AShiningRay <genesis45.ppc@gmail.com>
2024-08-20 19:20:58 -0300

    h850: Move to kernel config fragments
    Change-Id: I64126dbacde02a1666423f12f2276ad44dd303b8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ DEVICE_PATH := device/lge/h850
 TARGET_OTA_ASSERT_DEVICE := g5,h1,h850
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_h850_defconfig
+TARGET_KERNEL_CONFIG := vendor/lge/h850.config
 
 # Partitions
 # Override this after inheriting g5 common size
